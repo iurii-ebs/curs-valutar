7a2be5d65aa3d27cbb0f90f20c1b3a4b79aa2bba
alexjeman <alexandrujeman.ja@gmail.com>
2020-06-23 23:15:05 +0300

    Add view wallets and create wallet view(user authentication verification); Add corsheaders

--- apps/wallet/views.py
+++ apps/wallet/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from apps.wallet.models import (Currency,
+                                RatesHistory,
+                                Wallet,
+                                WalletOperation)
+
+from apps.wallet.serializers import (CurrencySerializer,
+                                     RatesHistorySerializer,
+                                     WalletSerializer,
+                                     WalletOperationSerializer)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes((IsAuthenticatedOrReadOnly,))
+@authentication_classes((JWTAuthentication,))
+def wallet_list(request):
+    if request.method == 'GET':
+        queryset = Wallet.objects.all()
+        serializer = WalletSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        user = JWTAuthentication().authenticate(request)[0]
+        serializer = WalletSerializer(data=request.data)
+        if serializer.is_valid():
+            wallet_new = Wallet.objects.create(
+                user=user,
+                currency=Currency.objects.get(abbr=serializer.validated_data['currency'])
+            )
+            wallet_new.save()
+            return Response(WalletSerializer(wallet_new).data)
+        return Response(serializer.errors)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def wallet_detail(request, pk):
+    pass
--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("apps.users.urls")),
+    path('api/', include("apps.wallet.urls")),
 ]
