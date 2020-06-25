866f49f7a8156d4195fc34c384ebcc37ad8781ec
artiom <ordersone@gmail.com>
2020-06-25 18:16:30 +0300

    Rates history progress

--- apps/statistics/views.py
+++ apps/statistics/views.py
@@ -1,5 +1,12 @@
-from rest_framework.decorators import api_view
+from datetime import datetime, timedelta
+
+
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+
+from apps.wallet.models import RatesHistory
+from apps.wallet.serializers import RatesHistorySerializer
 
 
 @api_view(['GET', 'POST'])
@@ -10,3 +17,18 @@ def predict_list(request):
 @api_view(['GET', 'POST'])
 def predict_detail(request, pk):
     pass
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def progress_detail_view(request, pk, days=7):
+    date_to = datetime.today()
+    date_from = date_to - timedelta(days=7)
+
+    queryset = RatesHistory.objects.filter(
+        currency=pk,
+        date__range=[date_from, date_to]
+    )
+    serializer = RatesHistorySerializer(queryset, many=True)
+
+    return Response(serializer.data)
