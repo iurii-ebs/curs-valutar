b6ecc924cf090706cf8edb0760458f24c6ea0aa7
alexjeman <alexandrujeman.ja@gmail.com>
2020-07-01 12:17:16 +0300

    Install swagger, documentation on / endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.permissions import AllowAny
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Curs Valutar API Documentation",
+        default_version='v1',
+        description="Curs  valutar API documentation",
+    ),
+    validators=['ssv'],
+    public=True,
+    permission_classes=(AllowAny,)
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +23,5 @@ urlpatterns = [
     path('wallets/', include("apps.wallet.urls")),
     path('statistics/', include("apps.statistics.urls")),
     path('notifications/', include('notifications.urls', namespace='notifications')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
