f14fe59f222bb591151c5b474ab2bef2f0275eb3
alexjeman <contact@alexjeman.com>
2020-07-09 14:39:07 +0300

    Set static settings to run with gunicorn/supervisor

--- config/settings.py
+++ config/settings.py
@@ -174,7 +174,6 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
-
 # Bank parser stuff
 BANK_PARSER_HOST = os.getenv('BANK_PARSER_HOST')
 BANK_PARSER_PORT = os.getenv('BANK_PARSER_PORT')
@@ -220,11 +219,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = "/static/"
+STATIC_ROOT = "/var/www/static"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Swagger
 SWAGGER_SETTINGS = {
