dafda0508744ecf03bc757a2da3ba346ad422a4a
alexjeman <contact@alexjeman.com>
2020-07-17 23:49:25 +0300

    fix pdf template, move static

--- apps/reports/views.py
+++ apps/reports/views.py
@@ -3,50 +3,14 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from wkhtmltopdf.views import PDFTemplateResponse
 import datetime
 from django.conf import settings
-import os
-from apps.reports.tasks import gen_static_graphs_all, save_pdf_report_files
+from apps.reports.tasks import gen_static_graphs_all
 from apps.wallet.serializers import RatesHistorySerializer
 from apps.statistics.models import RatesPredictionText
 from django.http import HttpResponse
-
-
-class PDFReportView(GenericAPIView):
-    authentication_classes = (JWTAuthentication,)
-    permission_classes = (AllowAny,)
-    template = 'reports/index.html'
-    context = {}
-
-    def get(self, request, pk):
-        forecast = RatesPredictionText.objects.get(currency_id=pk)
-        filename = f"currency_id_{forecast.currency.id}_{forecast.currency.abbr}_{forecast.currency.name}_{forecast.currency.bank}_{datetime.date.today()}.pdf".lower().replace(
-            " ", "_")
-        filepath = f"{settings.STATIC_ROOT}/pdf/"
-        check_pdf_available = os.path.isfile(filepath + filename)
-        if check_pdf_available:
-            with open(filepath + filename, 'rb') as pdf:
-                response = HttpResponse(pdf.read(), content_type='application/pdf')
-                response['Content-Disposition'] = f'filename={filename}'
-                return response
-        else:
-            save_pdf_report_files(pk, filepath, filename)
-
-            self.context = {"currency_id": str(pk),
-                            "period": f"{datetime.date.today()} / \
-                                       {datetime.date.today() + datetime.timedelta(7)}",
-                            "forecast": forecast.message}
-
-            response = PDFTemplateResponse(request=request,
-                                           template=self.template,
-                                           filename=filename,
-                                           context=self.context,
-                                           show_content_in_browser=True,
-                                           cmd_options={'enable-local-file-access': True}
-                                           )
-
-            return response
+from django.template.loader import get_template
+import pdfkit
 
 
 class GenPDFGraphs(GenericAPIView):
@@ -60,27 +24,36 @@ class GenPDFGraphs(GenericAPIView):
         return Response(status=status.HTTP_202_ACCEPTED)
 
 
-class PDFFileRenderView(GenericAPIView):
+class PDFReportViewNew(GenericAPIView):
+    queryset = ''
     authentication_classes = (JWTAuthentication,)
     permission_classes = (AllowAny,)
     template = 'reports/index.html'
     context = {}
+    serializer_class = RatesHistorySerializer
 
     def get(self, request, pk):
         forecast = RatesPredictionText.objects.get(currency_id=pk)
         filename = f"currency_id_{forecast.currency.id}_{forecast.currency.abbr}_{forecast.currency.name}_{forecast.currency.bank}_{datetime.date.today()}.pdf".lower().replace(
             " ", "_")
+        filepath = f"{settings.STATIC_ROOT}/pdf/"
+        fake_static = f"{settings.STATIC_ROOT}graphs/"
 
-        self.context = {"currency_id": str(pk),
+        self.context = {"currency_id": fake_static + str(pk),
                         "period": f"{datetime.date.today()} / \
                                    {datetime.date.today() + datetime.timedelta(7)}",
-                        "forecast": forecast.message}
+                        "forecast": forecast.message,
+                        "fake_static": fake_static}
+        template = get_template('reports/index.html')
+        html = template.render(self.context)
+        options = {
+            'page-size': 'Letter',
+            'encoding': "UTF-8",
+            'enable-local-file-access': ""
+        }
 
-        response = PDFTemplateResponse(request=request,
-                                       template=self.template,
-                                       filename=filename,
-                                       context=self.context,
-                                       show_content_in_browser=False,
-                                       cmd_options={'enable-local-file-access': True}
-                                       )
+        pdfkit.from_string(html, filepath + filename, options)
+        pdf = pdfkit.from_string(html, False, options)
+        response = HttpResponse(pdf, content_type='application/pdf')
+        response['Content-Disposition'] = f'filename={filename}'
         return response
