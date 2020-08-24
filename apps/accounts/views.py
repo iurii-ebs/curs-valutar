e449401d0db8d996ebe098c8783f0cd4fa480dd8
artiom <ordersone@gmail.com>
2020-08-24 11:15:14 +0300

    Removed firebase test endpoints (that depends py pyrebase)

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,19 +1,14 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import AllowAny
-from rest_framework.authentication import get_authorization_header
 
 from rest_framework_simplejwt.tokens import RefreshToken
 
-import json
-import pyrebase
-import firebase_admin
-from firebase_admin import credentials, auth
-from firebase_admin.exceptions import FirebaseError
+from firebase_admin import credentials, auth, initialize_app
 from firebase_admin.auth import (
     InvalidIdTokenError,
     ExpiredIdTokenError,
@@ -29,10 +24,7 @@ User = get_user_model()
 EMAIL_VERIFICATION = False
 
 firebase_admin_config = credentials.Certificate('firebase-admin-config.json')
-admin_app = firebase_admin.initialize_app(firebase_admin_config)
-
-firebase_config = json.load(open('firebase-config.json'))
-sdk_app = pyrebase.initialize_app(firebase_config)
+admin_app = initialize_app(firebase_admin_config)
 
 
 class FirebaseTokenView(CreateModelMixin, GenericAPIView):
@@ -95,66 +87,3 @@ class FirebaseTokenView(CreateModelMixin, GenericAPIView):
             'refresh': str(refresh),
             'access': str(refresh.access_token),
         })
-
-
-class FirebaseSignupView(CreateModelMixin, GenericAPIView):
-    """ Create account in firebase using email and password. FOR TESTS ONLY. Soon will be removed. """
-    permission_classes = [AllowAny]
-    serializer_class = serializers.FirebaseSignupSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors)
-
-        try:
-            user = auth.create_user(
-                email=serializer.validated_data['email'],
-                password=serializer.validated_data['password'],
-            )
-        except ValueError as e:
-            return Response(str(e))
-
-        except FirebaseError as e:
-            return Response(str(e))
-
-        return Response(f'Created user with uid: {user.uid}')
-
-
-class FirebaseSigninView(CreateModelMixin, GenericAPIView):
-    """ Obtain firebase token using email and password. FOR TESTS ONLY. Soon will be removed. """
-    permission_classes = [AllowAny]
-    serializer_class = serializers.FirebaseSigninSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors)
-
-        try:
-            user = sdk_app.auth().sign_in_with_email_and_password(
-                email=serializer.validated_data['email'],
-                password=serializer.validated_data['password'],
-            )
-            token = user['idToken']
-        except Exception as e:
-            return Response(str(e))
-
-        return Response({'token': token})
-
-
-class FirebaseHeaderView(RetrieveModelMixin, GenericAPIView):
-    """ Return Authorization header """
-    permission_classes = [AllowAny]
-
-    def get(self):
-        print('Request data: ', self.request.data)
-        print('Request query: ', self.request.query_params)
-        print('Request auth:', self.request.auth)
-        print('Request user:', self.request.user)
-        print('Request authenticators:', self.request.authenticators)
-
-        token = get_authorization_header(self.request)
-        return Response({'token': token})
