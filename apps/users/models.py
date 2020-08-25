51ea78c43eb1828269227ebadb095264165043a8
alexjeman <contact@alexjeman.com>
2020-08-25 11:28:05 +0300

    Add user alert preferences model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+from apps.commons.models import BaseModel
+
+
+class AlertPreference(BaseModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    alert_down = models.IntegerField(null=True, blank=True, default=3)
+    alert_down_forecast = models.IntegerField(null=True, blank=True, default=3)
