8d00d8acd711209f9d6068b191ee7595d85ae976
alexjeman <contact@alexjeman.com>
2020-08-25 11:29:39 +0300

    Upgrade admin panel views

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.users.models import AlertPreference
+
+
+@admin.register(AlertPreference)
+class AlertPreferenceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'alert_down', 'alert_down_forecast',)
+    search_fields = ('id', 'user__username', 'alert_down', 'alert_down_forecast',)
--- apps/wallet/admin.py
+++ apps/wallet/admin.py
@@ -7,8 +7,32 @@ from apps.wallet.models import (
     Wallet,
     WalletOperation)
 
-admin.site.register(Bank)
-admin.site.register(Currency)
-admin.site.register(RatesHistory)
-admin.site.register(Wallet)
-admin.site.register(WalletOperation)
+
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    list_display = ('id', 'registered_name', 'short_name', 'website', 'logo_path',)
+    search_fields = ('id', 'registered_name', 'short_name', 'website', 'logo_path',)
+
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'bank', 'name', 'abbr',)
+    search_fields = ('id', 'bank', 'name', 'abbr',)
+
+
+@admin.register(RatesHistory)
+class RatesHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'currency', 'rate_sell', 'rate_buy', 'date',)
+    search_fields = ('id', 'currency', 'rate_sell', 'rate_buy', 'date',)
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'currency',)
+    search_fields = ('id', 'user', 'currency',)
+
+
+@admin.register(WalletOperation)
+class WalletOperationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'wallet', 'currency', 'rate', 'amount',)
+    search_fields = ('id', 'wallet', 'currency', 'rate', 'amount',)
